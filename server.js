e41a5a9c813ad310c5a5eb4c5b9cf4d48a896cae
xavieracostapolo <xavieracosta@ingenieros.com>
2016-06-05 09:52:02 -0500

    Ajuste a json de respuesta

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require('express');
-var fs = require('fs');
 var querystring = require('querystring');
 var request = require('request');
 var cheerio = require('cheerio');
 var app = express();
 
-app.get('/tragolegal', function(req, res){
-   
+app.get('/v1/tragolegal/:codigo', function(req, res){
+   var codigo = req.params.codigo;
    var url = 'http://www.syctrace.com.co/testWs.aspx';
    
    var form = {
-       codiSun : '3CM25KWTT1'
+       codiSun : codigo.toUpperCase() //'3CM25KWTT1'
    };
    
    var formData = querystring.stringify(form);
@@ -36,12 +35,12 @@ app.get('/tragolegal', function(req, res){
    }, function(error, response, html){
        
         res.setHeader('Content-Type', 'application/json');
-                    
-        var procesado, mensaje, codigo;
+                 
         var jsonResponse = {
             procesado : false,
             mensaje : '',
-            codigo : form.codiSun
+            codigo : form.codiSun,
+            logError : ''
         };
             
        if(!error){
@@ -51,79 +50,27 @@ app.get('/tragolegal', function(req, res){
             $("input[name$='txtXmlMsg']").filter(function () {
                 var data = $(this);
                 var dato = data.val();
-                jsonResponse.mensaje = dato.substring(dato.indexOf("<normal>") + 8, dato.indexOf("</normal>") - dato.indexOf("<normal>") + 8);
-                console.log(dato);
+                var autorizado = dato.indexOf("no autorizado") <= -1;// si no esta la palabra
+                if(autorizado){
+                    jsonResponse.procesado = true;
+                    jsonResponse.mensaje = dato.substring(dato.indexOf("<normal>") + 8, dato.indexOf("</normal>") - dato.indexOf("<normal>") + 8);    
+                }
+                else{
+                    jsonResponse.procesado = false;
+                    jsonResponse.mensaje = 'Código no autorizado, verifíquelo y escríbalo nuevamente.';
+                }
             })
             
-            jsonResponse.procesado = 'true';
             res.send(JSON.stringify(jsonResponse));
              
        }
        else{
-           console.log(error);
            jsonResponse.mensaje = 'Error procesando la solicitud';
+           jsonResponse.logError = error;
            res.send(JSON.stringify(jsonResponse));
        }
    });
    
-});
-
-app.get('/scrape', function(req, res){
-    
-    // The URL we will scrape from - in our example Anchorman 2.
-
-    url = 'http://www.imdb.com/title/tt1229340/';
-
-    // The structure of our request call
-    // The first parameter is our URL
-    // The callback function takes 3 parameters, an error, response status code and the html
-
-    request(url, function(error, response, html){
-
-        // First we'll check to make sure no errors occurred when making the request
-        // res.send(html);
-        if(!error){
-            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
-
-            var $ = cheerio.load(html);
-
-            // Finally, we'll define the variables we're going to capture
-
-            var title, release, rating;
-            var json = { title : "", release : "", rating : ""};
-            
-            $('.header').filter(function(){
-                var data = $(this);
-                title = data.children().first().text();
-            
-                release = data.children().last().children().text();
-
-                json.title = title;
-                json.release = release;
-            })
-
-            // Since the rating is in a different section of the DOM, we'll have to write a new jQuery filter to extract this information.
-
-            $('.star-box-giga-star').filter(function(){
-                var data = $(this);
-
-                // The .star-box-giga-star class was exactly where we wanted it to be.
-                // To get the rating, we can simply just get the .text(), no need to traverse the DOM any further
-
-                rating = data.text();
-
-                json.rating = rating;
-            });
-            
-            res.setHeader('Content-Type', 'application/json');
-            res.send(JSON.stringify(json));
-            
-        }
-        else{
-            console.log(error);
-        }
-    })
-    
 });
 
 app.listen('8081');
